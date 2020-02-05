8c32bb521388efc1cc9979034b558d3038e8a1bb
eguahlak <anders@kalhauge.dk>
2020-02-05 11:16:21 +0100

    main

--- src/main/java/dk/cphbusiness/alg/basics/QuickFind.java
+++ src/main/java/dk/cphbusiness/alg/basics/QuickFind.java
@@ -34,4 +34,27 @@ public class QuickFind implements UnionFind{
         return count;
         }
 
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < pointSets.length; i++) {
+            result+=pointSets[i]+" ";
+            }
+        return result;
+        }
+
+    public static void main(String[] args) {
+        QuickFind qf = new QuickFind(10);
+        qf.union(7, 8);
+        System.out.println(qf.toString());
+        qf.union(5, 6);
+        System.out.println(qf.toString());
+        qf.union(5, 7);
+        System.out.println(qf.toString());
+        qf.union(1, 0);
+        System.out.println(qf.toString());
+        qf.union(7, 0);
+        System.out.println(qf.toString());
+        }
+
     }
